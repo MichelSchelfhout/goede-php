00e52413ba1d561814e705c3bae4e88ae064c88d
MichelSchelfhout <31687630+MichelSchelfhout@users.noreply.github.com>
2018-02-06 12:05:56 +0100

    Eerste commit. Opdracht is klaar. Vrij makkelijk.

--- index.php
+++ index.php
@@ -1,10 +1,48 @@
+
+	<?php
+
+	// Tijdzone // 
+
+	date_default_timezone_set('europe/amsterdam');
+        $morning = 6;
+        $afternoon = 12;
+        $evening = 18;
+        $night= 0;
+        $hour = date("H", time());
+
+         if($hour >= $morning && $hour < $afternoon){
+            $class = "morning";
+            echo "<p>Goede Morgen! <br>Het is nu: " . date("H:i", time()) . "</p>" ;
+        }
+
+        if($hour >= $afternoon && $hour < $evening){
+            $class = "afternoon";
+            echo "<p>Goede Middag! <br>Het is nu: " . date("H:i", time()) . "</p>";
+        }
+
+        if($hour >= $evening && $hour < $night){
+            $class = "evening";
+            echo "<p>Goede Avond! <br>Het is nu: " . date("H:i", time()) . "</p>";
+        }
+
+        if($hour >= $night && $hour < $morning){
+            $class = "night";
+            echo "<p>Goede Nacht! <br>Het is nu: " . date("H:i", time()) . "</p>";
+        }
+
+        ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+    <title>Goede</title>
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <meta charset="utf-8">
 </head>
-<body>
-	
+<body class="<?=$class?>">
+
+
+
+
 </body>
 </html>
